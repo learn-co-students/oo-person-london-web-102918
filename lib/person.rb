5b3bfaa6372bcbd20c0920d21492e0ecc0f248a6
seeduardo <romenflatiron@gmail.com>
2018-10-31 14:03:09 +0000

    some extra stuff?

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,35 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+    #attr_accessor :bank_account, :happiness, :hygiene
+    #attr_reader :name
+    #hello this is some stuff and stuff
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def name
+    @name
+  end
+
+  def bank_account
+    @bank_account
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def happy?
+
+
+  def hygiene
+    @hygiene
+  end
+
+end
