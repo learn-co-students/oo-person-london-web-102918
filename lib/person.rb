60590c4d6bd28e9722f2ad336bbb72e2c9d8d4b2
seeduardo <romenflatiron@gmail.com>
2018-10-31 13:51:38 +0000

    Add Class Method for Person, Initialized with Name and Bank, Happiness and Hygiene

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+    #attr_accessor :bank_account, :happiness, :hygiene
+    #attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def name
+    @name
+  end
+
+  def bank_account
+    @bank_account
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def hygiene
+    @hygiene
+  end
+
+end
